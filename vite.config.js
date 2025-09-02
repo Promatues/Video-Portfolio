025189bc67135d8fa6db58bfcc51f84e8522044a
Glenniel <Glennielgutang1@gmail.com>
2025-09-02 12:09:39 +0800

    Final fix?

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Video-Edit-Portfolio/', // <-- Add this line
+  base: '/Video-Portfolio/', // <-- Add this line
 })
